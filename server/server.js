cb19545842d72cc87d3dcdb0d7e4388e557a8c8c
VEERENDRA VAMSHI <136776337+MKVEERENDRA@users.noreply.github.com>
2025-02-06 23:01:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const usersRoutes = require("./routes/users.js");
 const transactionsRoutes = require("./routes/Transactions.js");
 const userPortfolio = require("./routes/userPortfolio.js");
 const mongoose = require("mongoose");
+const ipfsRoutes = require("./routes/ipfs.js");
 const cors = require("cors");
 
 dotenv.config();
@@ -34,6 +35,7 @@ app.use("/api/workouts/", workoutRoutes);
 app.use("/api/portfolio/", userPortfolio);
 app.use("/api/transactions/", transactionsRoutes);
 app.use("/api/users/", usersRoutes);
+app.use("/api/ipfs/", ipfsRoutes);
 
 //connect to db et lancement du server
 mongoose
